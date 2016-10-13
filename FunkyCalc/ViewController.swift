d7ee4ff7b7042317497b597994f4192f8173095a
hostname <github@elliotsanchez.com>
2016-10-13 13:36:06 -0500

    initial commit with all buttons, actions, and functions setup, as well as layout in stacked views

--- FunkyCalc/ViewController.swift
+++ FunkyCalc/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // Button Actions
+    @IBAction func ClickNumber (_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func ClickOperator (_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func ClickEquals (_ sender: UIButton) {
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
