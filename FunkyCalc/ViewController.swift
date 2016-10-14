00ed261d4507f2057d52d3e72fa7691ae3a0a315
hostname <github@elliotsanchez.com>
2016-10-14 11:16:44 -0500

    added updateDisplay function with logic for managing input numbers, called function on clickNumber buttons.

--- FunkyCalc/ViewController.swift
+++ FunkyCalc/ViewController.swift
@@ -15,8 +15,7 @@ class ViewController: UIViewController {
     var calcState :CalculationState = CalculationState.enteringNum
     
     // Variables for calculation logic
-    var firstValue = String()
-    
+    var firstValue :String = ""
     
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -24,14 +23,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    // Button Actions
-    @IBAction func clickNumber (_ sender: UIButton) {
-        
-        
+    
+    // Button actions
+    @IBAction func clickNumber(_ sender: UIButton) {
+        updateDisplay(number :String(sender.tag))
     }
     
-    @IBAction func clickOperator (_ sender: UIButton) {
+    @IBAction func clickOperator(_ sender: UIButton) {
+        //calcState = CalculationState.newNumStarted
+        
         switch sender.tag {
         case 50:
             print("DEBUG add")
@@ -47,10 +47,24 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func clickEquals (_ sender: UIButton) {
+    @IBAction func clickEquals(_ sender: UIButton) {
         
     }
     
+    // Display actions and logic
+    func updateDisplay (number :String) {
+        if calcState == CalculationState.newNumStarted {
+            if let num = resultLabel.text {
+                if num != "" {
+                    firstValue = num
+                }
+            }
+            calcState = CalculationState.enteringNum
+            resultLabel.text = number
+        } else if calcState == CalculationState.enteringNum {
+            resultLabel.text = resultLabel.text! + number
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
